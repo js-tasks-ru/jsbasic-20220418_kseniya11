ca031bff52e21f70efbd30965287bd6afd4c74b6
kchebotova <kchebotova@yandex-team.ru>
2022-05-04 14:26:45 +0300

    решение задач модуля 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,7 +1,7 @@
 function camelize(str) {
   let arr = str.split('-');
   let arr2 = arr.map(
-    (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    (word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)
   );
   return arr2.join('');
 }
